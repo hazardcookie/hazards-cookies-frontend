c6d8686e2776cd060fb7a23347d2dc178ba83c1a
hazaredcookie <wwallace@ripple.com>
2023-02-19 23:22:46 -0500

    formatted

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,86 +1,86 @@
 <script lang="ts">
-	import '../app.css';
-	import { contract } from '../constants';
-	import cookies from './cookie_data.json';
-	import { fly } from 'svelte/transition';
-	const cookie_titles = Object.keys(cookies.cookies);
-	const cookie_data = Object.values(cookies.cookies);
-	const cookie_contract = contract;
+  import '../app.css';
+  import { contract } from '../constants';
+  import cookies from './cookie_data.json';
+  import { fly } from 'svelte/transition';
+  const cookie_titles = Object.keys(cookies.cookies);
+  const cookie_data = Object.values(cookies.cookies);
+  const cookie_contract = contract;
 </script>
 
 <div class="dapp_container">
-	<div class="rules" transition:fly={{ y: -200, duration: 2000 }}>
-		<p class="cookie_title">Hazard's Cookies 🍪</p>
-		{#each cookie_titles as cookie, i}
-			<p>
-				<span class="cookie_titles">{cookie}</span>: {cookie_data[i]}
-			</p>
-		{/each}
+  <div class="rules" transition:fly={{ y: -200, duration: 2000 }}>
+    <p class="cookie_title">Hazard's Cookies 🍪</p>
+    {#each cookie_titles as cookie, i}
+      <p>
+        <span class="cookie_titles">{cookie}</span>: {cookie_data[i]}
+      </p>
+    {/each}
 
-		<p>
-			<a href="https://evm-sidechain.xrpl.org/address/{cookie_contract}">{cookie_contract}</a>
-		</p>
-	</div>
+    <p>
+      <a href="https://evm-sidechain.xrpl.org/address/{cookie_contract}">{cookie_contract}</a>
+    </p>
+  </div>
 
-	<slot class="cookie_cards" />
+  <slot class="cookie_cards" />
 </div>
 
 <style>
-	a {
-		color: white;
-		text-decoration: none;
-		transition: 0.5s;
-		font-size: 0.9rem;
-	}
-	a:hover {
-		color: #c1c1c1;
-		text-decoration: none;
-	}
-	.cookie_title {
-		font-size: 2.5rem;
-		font-weight: 700;
-	}
-	.rules {
-		border-radius: 10px;
-		margin: 10px;
-	}
-	.cookie_titles {
-		margin: 0;
-		padding: 0;
-		font-size: 1.2rem;
-		font-weight: 700;
-	}
-	.dapp_container {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		width: 100%;
-	}
-	.cookie_cards {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		width: 100%;
-	}
-	/* mobile friendly */
-	@media only screen and (min-width: 900px) {
-		.dapp_container {
-			flex-direction: row;
-		}
-		.cookie_cards {
-			flex-direction: row;
-			flex-direction: column;
-		}
-		.rules {
-			width: 100%;
-		}
-	}
-	@media only screen and (max-width: 600px) {
-		.cookie_title {
-			font-size: 1.7rem;
-		}
-		a {
-			font-size: 0.8rem;
-		}
-	}
+  a {
+    color: white;
+    text-decoration: none;
+    transition: 0.5s;
+    font-size: 0.9rem;
+  }
+  a:hover {
+    color: #c1c1c1;
+    text-decoration: none;
+  }
+  .cookie_title {
+    font-size: 2.5rem;
+    font-weight: 700;
+  }
+  .rules {
+    border-radius: 10px;
+    margin: 10px;
+  }
+  .cookie_titles {
+    margin: 0;
+    padding: 0;
+    font-size: 1.2rem;
+    font-weight: 700;
+  }
+  .dapp_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+  }
+  .cookie_cards {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+  }
+  /* mobile friendly */
+  @media only screen and (min-width: 900px) {
+    .dapp_container {
+      flex-direction: row;
+    }
+    .cookie_cards {
+      flex-direction: row;
+      flex-direction: column;
+    }
+    .rules {
+      width: 100%;
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    .cookie_title {
+      font-size: 1.7rem;
+    }
+    a {
+      font-size: 0.8rem;
+    }
+  }
 </style>
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,20 @@
 export interface Cookie_metadata {
-	name: string;
-	image: string;
+  name: string;
+  image: string;
 }
 
 export type Cookie_collection = {
-	Power: { owner: string; metadata: Cookie_metadata };
-	Wisdom: { owner: string; metadata: Cookie_metadata };
-	Time: { owner: string; metadata: Cookie_metadata };
-	War: { owner: string; metadata: Cookie_metadata };
-	Wealth: { owner: string; metadata: Cookie_metadata };
+  Power: { owner: string; metadata: Cookie_metadata };
+  Wisdom: { owner: string; metadata: Cookie_metadata };
+  Time: { owner: string; metadata: Cookie_metadata };
+  War: { owner: string; metadata: Cookie_metadata };
+  Wealth: { owner: string; metadata: Cookie_metadata };
 };
 
 export type Owners = {
-	Power: string;
-	Wisdom: string;
-	Time: string;
-	War: string;
-	Wealth: string;
+  Power: string;
+  Wisdom: string;
+  Time: string;
+  War: string;
+  Wealth: string;
 };
