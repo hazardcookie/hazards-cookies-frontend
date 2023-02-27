914269708ff6eea224f093c2adb387a097edde39
hazaredcookie <wwallace@ripple.com>
2023-02-26 21:23:08 -0500

    update contract to v5

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,4 +1,4 @@
-export const contract = '0xDA8969c0E047e12bfda6281283C7A7Bfdd0B70F5';
+export const contract = '0x392F814DA5c3C0B9e8ee2742C8c85839e9171989';
 export const rpc = 'https://rpc-evm-sidechain.xrpl.org';
 // export const rpc = 'http://127.0.0.1:8545';
 // export const contract = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
