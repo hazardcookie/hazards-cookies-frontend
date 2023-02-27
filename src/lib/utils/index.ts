ba957c3786a6d569aa6ae4d86221f66c1590c1e4
hazaredcookie <wwallace@ripple.com>
2023-02-26 21:48:49 -0500

    lengthen owner

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,4 +1,4 @@
 // shortens eth address
 export function eth_address_shortener(address: string) {
-  return address.slice(0, 7) + '...' + address.slice(-3);
+  return address.slice(0, 10) + '...';
 }
