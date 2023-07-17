1f2eccb2d2194298a4079ebb2c7c353d92ab18a8
hazaredcookie <wwallace@ripple.com>
2023-07-17 12:56:14 -0400

    contract update

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,4 +1,4 @@
-export const contract = '0xb43007FC420eDF2a50199F3A4D9b158050bb8b28';
+export const contract = '0x40aa6d128796AdD6Faa88d97D7a97B42BBA17888';
 export const rpc = 'https://rpc-evm-sidechain.xrpl.org';
 // export const rpc = 'http://127.0.0.1:8545';
 // export const contract = '0x5fbdb2315678afecb367f032d93f642f64180aa3';
